eb276408653dc2abfa81cc1b7b0b8b4f8e4648d9
Pariwesh <tamrpariwesh@gmail.com>
2022-04-04 23:53:38 +1000

    added height to product list area

--- src/screens/Product/ProductListScreenStyles.js
+++ src/screens/Product/ProductListScreenStyles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const ProductListScreenContainer = styled.div``
+export const ProductListScreenContainer = styled.div`
+  height: 67vh;
+`
 export const Title = styled.h1`
   margin: 20px;
   padding-left: 20px;
