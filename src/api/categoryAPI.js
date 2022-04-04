cad0425e3a578012cf68847340c34fb0974e27c1
Pariwesh <tamrpariwesh@gmail.com>
2022-04-04 23:28:28 +1000

    route path update

--- src/api/categoryAPI.js
+++ src/api/categoryAPI.js
@@ -3,7 +3,9 @@ import Axios from 'axios'
 const catApi = `${process.env.ROOT_URL}/api/v1/category`
 export const getCategories = async () => {
   try {
-    const { data } = await Axios.get(catApi)
+    const { data } = await Axios.get(
+      'https://mern-soccer-store.herokuapp.com/api/v1/category',
+    )
     return data
   } catch (error) {
     return error?.message?.data || { status: 'error', message: error.message }
