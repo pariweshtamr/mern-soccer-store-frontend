94dba6dd4e604a3e8ae6042d28731a6b66516fcd
pariwesh <tamrpariwesh@gmail.com>
2022-10-11 10:32:13 +1100

    changed route for products

--- src/api/productAPI.js
+++ src/api/productAPI.js
@@ -1,21 +1,21 @@
-import Axios from 'axios'
+import Axios from "axios"
+const url =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_ROOT_URL
+    : "http://localhost:8000/api/v1"
 
 export const getProducts = async () => {
   try {
-    const { data } = await Axios.get(
-      'https://mern-soccer-store.herokuapp.com/api/v1/products',
-    )
+    const { data } = await Axios.get(url + "/products")
     return data
   } catch (error) {
-    return error?.message?.data || { status: 'error', message: error.message }
+    return error?.message?.data || { status: "error", message: error.message }
   }
 }
 
 export const getAProductById = async (_id) => {
   try {
-    const { data } = await Axios.get(
-      `https://mern-soccer-store.herokuapp.com/api/v1/products/${_id}`,
-    )
+    const { data } = await Axios.get(url + `/products/${_id}`)
     return data
   } catch (error) {
     console.log(error)
