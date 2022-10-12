f2ff95c3ed4c82c3143bad8feb8c93a04791e8ff
pariwesh <tamrpariwesh@gmail.com>
2022-10-12 14:36:58 +1100

    edited url in products page

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import LoadingBox from '../LoadingBox/LoadingBox'
-import MessageBox from '../MessageBox/MessageBox'
-import Axios from 'axios'
-import ProductItem from './ProductItem'
-import { ProductsContainer, ProductsListContainer } from './ProductStyles'
+import React, { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import LoadingBox from "../LoadingBox/LoadingBox"
+import MessageBox from "../MessageBox/MessageBox"
+import Axios from "axios"
+import ProductItem from "./ProductItem"
+import { ProductsContainer, ProductsListContainer } from "./ProductStyles"
 
 const Products = ({ cat, filters }) => {
   const [products, setProducts] = useState([])
   const [filteredProducts, setFilteredProducts] = useState([])
 
   const { isLoading, productResponse } = useSelector(
-    (state) => state.product || [],
+    (state) => state.product || []
   )
 
+  const url =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_ROOT_URL
+      : "http://localhost:8000/api/v1"
   useEffect(() => {
     const getProducts = async () => {
       try {
         const result = await Axios.get(
-          cat
-            ? `https://mern-soccer-store.herokuapp.com/api/v1/products?category=${cat}`
-            : 'https://mern-soccer-store.herokuapp.com/api/v1/products',
+          cat ? url + "/products?category=${cat}" : `${url}/products`
         )
         setProducts(result.data)
-      } catch (error) {}
+      } catch (error) {
+        console.log(error)
+      }
     }
     getProducts()
   }, [cat])
@@ -33,9 +37,9 @@ const Products = ({ cat, filters }) => {
       setFilteredProducts(
         products.filter((product) =>
           Object.entries(filters).every(([key, value]) =>
-            product[key].includes(value),
-          ),
-        ),
+            product[key].includes(value)
+          )
+        )
       )
   }, [products, cat, filters])
 
